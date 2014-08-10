d92dbda2087fe50a42f8856340aa168e7dfcbe1e
Marcin Smoczynski <smoczynski.marcin@gmail.com>
2014-08-10 17:59:49 +0200

    install ldscripts to lib, headers to include but unpack to tmpdir

--- install_gccrh_files.sh
+++ install_gccrh_files.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # check arguments
 if [ ! -d "${1}" -o ! -f "${2}" ]; then
@@ -9,17 +9,28 @@ fi
 
 INSTALL_TARGET_PREFIX=`realpath "${1}"`
 PATH_TO_ZIP=`realpath "$2"`
+UNPACK_TMPDIR=`mktemp -d`
 
-cd $INSTALL_TARGET_PREFIX/msp430-elf
-unzip -u $PATH_TO_ZIP
+# generate directory structure if needed
+if [ ! -d "${INSTALL_TARGET_PREFIX}/msp430-elf" ]; then
+	echo "Creating directory structure"
+	mkdir -p ${INSTALL_TARGET_PREFIX}/msp430-elf/{include,lib/ldscripts}
+fi
+
+unzip -q "${PATH_TO_ZIP}" -d ${UNPACK_TMPDIR}
+
 # Header files should be inside include/, but also a boatload of directories containing the linker scripts; they need
 # to be combined and stored into lib/<part#>.ld.
 
-cd include
+cd ${UNPACK_TMPDIR}/include
 echo "Generating ldscripts-"
-for i in $(find . -maxdepth 1 -name 'msp430*' -type d | sed -e 's/^\.\///') $(find . -maxdepth 1 -name 'cc430*' -type d | sed -e 's/^\.\///') ; do
+for i in $(find . -maxdepth 1 \( -name 'msp430*' -o -name 'cc430*' \) -type d | sed -e 's/^\.\///') ; do
         echo "Processing $i..."
-        cat ${i}/memory.ld ${i}/peripherals.ld > ../lib/ldscripts/${i}.ld
-        rm -f ${i}/*.ld
-        rmdir ${i}
+        cat ${i}/memory.ld ${i}/peripherals.ld > ${INSTALL_TARGET_PREFIX}/msp430-elf/lib/${i}.ld
 done
+
+# install headers
+cp *.h ${INSTALL_TARGET_PREFIX}/msp430-elf/include
+find . -name "*.ld" | xargs -I "{}" cp "{}" ${INSTALL_TARGET_PREFIX}/msp430-elf/lib/
+
+rm -rf ${UNPACK_TMPDIR}
